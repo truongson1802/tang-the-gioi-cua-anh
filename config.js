e08ee7e527598a43f517ac086ccce9db5755fef6
truongson1802 <truongsonxbaochau1309@gmail.com>
2021-10-17 22:15:13 +0700

    infit file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 hiiii em 🖐🖐🖐',
+    introDesc: `Em chuẩn bị tinh thần cho đều anh sắp nói nhaaaaa`,
+    btnIntro: 'OK',
+    title: 'Làm người yêu anh nha bé ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :3 ',
+    btnYes: 'Có, em rấtttttttt thích anh 💖💖💖',
+    btnNo: '💔 Đồ điên!!!! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh rất tốt bụng và đẹp traiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/tamato.567/'
+}
